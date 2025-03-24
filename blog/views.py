f593acdf785732c88684581e65ba53f0ec7dd665
Raymond <raymondtdev@gmail.com>
2025-03-24 21:57:09 +0200

    Added CRUD functionality: Create, Update, Delete. Added TESTS for CRUD operations.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+  CreateView, 
+  UpdateView,
+  DeleteView
+)
+from django.urls import reverse_lazy
+
 from .models import Post
 
 # Create your views here.
@@ -9,3 +16,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
   model = Post
   template_name = 'post_detail.html'
+
+class BlogCreateNew(CreateView):
+  model = Post
+  template_name = 'create_post.html'
+  fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+  model = Post
+  template_name = 'edit_post.html'
+  fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+  model = Post
+  template_name = 'delete_post.html'
+  success_url = reverse_lazy('home')
